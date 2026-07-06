684ab402f93294c67b56a4b439d2ed7a014280f8
gahbaha <acklinshaun2@gmail.com>
2026-07-06 19:57:06 -0400

    US04: add level 1 tilemap, spikes trap, and player traversal

---
+++ actors/traps/spikes.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+const DAMAGE = 1
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		body.take_damage(DAMAGE)
